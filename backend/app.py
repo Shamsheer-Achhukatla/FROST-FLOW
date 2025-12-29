b25364e3d3574bd275d2db5b09551bc2585fc68e
Shamsheer Achhukatla <249x5a0399@gprec.ac.in>
2025-12-29 22:54:49 +0530

    Cleaned backend, fixed routes and structure

--- backend/app.py
+++ backend/app.py
@@ -1,55 +1,43 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, redirect
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from dotenv import load_dotenv
-from database import db
 import os
 
+# DB + ROUTES
+from database import db
 from routes.auth_routes import auth
 from routes.product_routes import product
 from routes.order_routes import order
 from routes.service_routes import service
 
-
 load_dotenv()
-
 app = Flask(__name__)
 
+# CONFIG
 app.config["JWT_SECRET_KEY"] = os.getenv("SECRET_KEY")
+JWTManager(app)
 
-FRONTEND_URL = os.getenv("FRONTEND_URL")
+# CORS / FRONTEND CONNECTION
 CORS(app,
      resources={r"/*": {"origins": os.getenv("FRONTEND_URL")}},
      supports_credentials=True)
 
-# Force HTTPS only on Render
+# HTTPS REDIRECT FOR RENDER
 @app.before_request
 def before_request():
     if not request.is_secure and "render" in request.host:
-        url = request.url.replace("http://", "https://", 1)
-        return redirect(url, code=301)
+        return redirect(request.url.replace("http://", "https://", 1), 301)
 
-JWTManager(app)
-
-# REGISTER ROUTES
+# ROUTES
 app.register_blueprint(auth, url_prefix="/auth")
 app.register_blueprint(product, url_prefix="/products")
 app.register_blueprint(order, url_prefix="/orders")
 app.register_blueprint(service, url_prefix="/service")
 
-# TEST ROUTE
 @app.get("/")
 def home():
     return jsonify({"message": "Frost & Flow Backend Running"}), 200
-
-@app.route("/test-db")
-def test_db():
-    try:
-        db.command("ping")
-        return jsonify({"message": "MongoDB Atlas connected successfully!"})
-    except:
-        return jsonify({"error": "Connection Failed"}), 500
-
 
 if __name__ == "__main__":
     from waitress import serve
--- backend/products.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify
-from database import products_collection
-
-products = Blueprint("products", __name__)
-
-@products.get("/products/all")
-def get_all_products():
-    data = list(products_collection.find({}, {"_id": 0}))
-    return jsonify(data), 200
